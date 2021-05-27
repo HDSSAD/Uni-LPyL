a77fa2b05ac503008cfe81227e6a6398cc4f1158
HDSSAD <42951238+HDSSAD@users.noreply.github.com>
2021-05-27 17:36:15 -0300

    Add files via upload

--- Ejercicio Integrador 2 - PHP/js/script.js
+++ Ejercicio Integrador 2 - PHP/js/script.js
@@ -4,16 +4,51 @@ let aInptTxtNum = document.getElementsByClassName("inpt-txtNum");
 let aInptNumero = document.getElementsByClassName("inpt-numero");
 let aInptTexto = document.getElementsByClassName("inpt-texto");
 
-let patron_texto = /^[a-zA-ZáéíóúÁÉÍÓÚ'\s]*$/;
-let patron_numero = /^[0-9]*$/;
+const patron_texto = /[^a-zA-ZáéíóúÁÉÍÓÚ'\s]/g;
+const patron_numero = /[^0-9]/g;
 
-let patron_txtNumLite = /^[a-zA-Z'\-\s0-9]*$/;
-let patron_txtNumGuion = /^[a-zA-ZáéíóúÁÉÍÓÚ'\-\s0-9]*$/;
+const patron_txtNumLite = /[^a-zA-Z'\-\s0-9]/g;
+const patron_txtNumGuion = /[^a-zA-ZáéíóúÁÉÍÓÚ'\-\s0-9]/g;
 
-let patron_txtNum = /^[a-zA-ZáéíóúÁÉÍÓÚ'\s0-9]*$/;
+const patron_txtNum = /[^a-zA-ZáéíóúÁÉÍÓÚ'\s0-9]/g;
 
 /* 
-No pude añadir los eventListener a los input utilizando las variables creadas al inicio del documento
-por alguna razon me me lo permitia y siempre aparecia un error en la consola al ejecutar la funcion despues del evento input
-Solo quedaron añadidas las validaciones html, y en php
- */
+Remueve los caracteres invalidos al momento en que se ingresan al textbox
+Permitiendo solo entradas validas segun el regex indicado
+*/
+
+for (let i = 0; i < aInptTexto.length; i++) {
+    let element = aInptTexto[i];
+    element.addEventListener("input", (e) => {
+        let validTxt = e.target.value.replace(patron_texto, "");
+        e.target.value = validTxt;
+    });
+}
+for (let i = 0; i < aInptNumero.length; i++) {
+    let element = aInptNumero[i];
+    element.addEventListener("input", (e) => {
+        let validTxt = e.target.value.replace(patron_numero, "");
+        e.target.value = validTxt;
+    });
+}
+for (let i = 0; i < aInptTxtNum.length; i++) {
+    let element = aInptTxtNum[i];
+    element.addEventListener("input", (e) => {
+        let validTxt = e.target.value.replace(patron_txtNum, "");
+        e.target.value = validTxt;
+    });
+}
+for (let i = 0; i < aInptTxtNumLite.length; i++) {
+    let element = aInptTxtNumLite[i];
+    element.addEventListener("input", (e) => {
+        let validTxt = e.target.value.replace(patron_txtNumLite, "");
+        e.target.value = validTxt;
+    });
+}
+for (let i = 0; i < aInptTxtNumGuion.length; i++) {
+    let element = aInptTxtNumGuion[i];
+    element.addEventListener("input", (e) => {
+        let validTxt = e.target.value.replace(patron_txtNumGuion, "");
+        e.target.value = validTxt;
+    });
+}
